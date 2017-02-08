d9650633083e79e5e4eed8259562285fee6fb13e
Serene Sun <serene@Serenes-MacBook-Pro.local>
2017-02-08 14:55:25 -0800

    update setup

--- setup.py
+++ setup.py
@@ -6,25 +6,25 @@ import ast
 # https://github.com/mitsuhiko/flask/blob/master/setup.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('q2_pfdr/__init__.py', 'rb') as f:
+with open('q2_dfdr/__init__.py', 'rb') as f:
     hit = _version_re.search(f.read().decode('utf-8')).group(1)
     version = str(ast.literal_eval(hit))
 
 setup(
-    name="q2-pfdr",
-    version=version,
+    name="q2-dfdr",
+    version='0.0.1',
     packages=find_packages(),
     # pandas and q2-dummy-types are only required for the dummy methods and
     # visualizers provided as examples. Remove these dependencies when you're
     # ready to develop your plugin, and add your own dependencies (if there are
     # any).
     install_requires=['qiime >= 2.0.5', 'pandas',
-                      'numpy', 'scipy'],
+                      'numpy', 'scipy', 'statsmodels'],
     author="Serene Jiang",
     author_email="serene1030@gmail.com",
-    description="Description of q2-pfdr",
+    description="Discrete FDR Method",
     entry_points={
         "qiime.plugins":
-        ["q2-pfdr=q2_pfdr.plugin_setup:plugin"]
+        ["q2-pfdr=q2_dfdr.plugin_setup:plugin"]
     }
 )
